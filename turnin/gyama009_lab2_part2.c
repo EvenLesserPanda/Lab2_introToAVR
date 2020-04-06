97ce14740a569ae810cdece63736de0f9ae179b1
Garrett Yamano <gyama009@wch136-25.cs.ucr.edu>
2020-04-06 13:55:32 -0700

    Completed part 2

---
+++ turnin/gyama009_lab2_part2.c
@@ -0,0 +1,24 @@
+/*	Author: gyama009
+ *  Partner(s) Name: 
+ *	Lab Section:
+ *	Assignment: Lab #  Exercise #
+ *	Exercise Description: [optional - include for your own benefit]
+ *
+ *	I acknowledge all content contained herein, excluding template or example
+ *	code, is my own original work.
+ */
+#include <avr/io.h>
+#ifdef _SIMULATE_
+#include "simAVRHeader.h"
+#endif
+
+int main(void) {
+	DDRA = 0x00; PORTA = 0xFF; // Configure port A's 8 pins as inputs
+	DDRC = 0xFF; PORTC = 0x00; // Configure port C's 8 pins as outputs
+	unsigned char cntavail = 0x00; // Temporary variable to hold # of taken spots
+	while(1){
+		cntavail = ((PINA & 0x08) >> 3) + ((PINA & 0x04) >> 2) + ((PINA & 0x02) >> 1) + (PINA & 0x01);
+		PORTC = 4 - cntavail;
+	}
+	return 0;
+}
