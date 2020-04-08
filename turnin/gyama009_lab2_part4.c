e8fa6f66c6f1fca943436af2c47b994b9517b6eb
Garrett Yamano <gyama009@wch136-25.cs.ucr.edu>
2020-04-07 23:54:03 -0700

    Seeing if Gradescope likes this

--- turnin/gyama009_lab2_part4.c
+++ turnin/gyama009_lab2_part4.c
@@ -18,52 +18,41 @@ int main(void) {
 	DDRB = 0x00; PORTB = 0xFF; // Configure port B's 8 pins as inputs
 	DDRC = 0x00; PORTC = 0xFF; // Configure port C's 8 pins as inputs
 	DDRD = 0xFF; PORTD = 0x00; // Configure port D's 8 pins as outputs
-	// unsigned char holdchar = 0x00; // Holding the exact weight
-	unsigned short holdchar = 0x0000;
-	unsigned short tempweight = 0x0000; // Holding the temp weight
+	unsigned char holdchar = 0x00;
+	unsigned char tempweight = 0x00; // Holding the temp weight
 	while(1){
 		holdchar = PINA + PINB + PINC;
-		//if((PINA + PINB + PINC) >= 512){
-		if(holdchar >= 512){
-			//holdchar = (PINA + PINB + PINC) - 512;
-			holdchar -= 512;
-			tempweight += 0x0200;
-		}
-		if(holdchar >= 256){
-			holdchar -= 256;
-			tempweight += 0x0100;
-		}
-		if(holdchar >= 128){
+		if(holdchar >= 0x80){
 			holdchar -= 128;
-			tempweight += 0x0080;
+			tempweight += 0x80;
 		}
-		if(holdchar >= 64){
+		if(holdchar >= 0x40){
 			holdchar -= 64;
-			tempweight += 0x0040;
+			tempweight += 0x40;
 		}
-		if(holdchar >= 32){
+		if(holdchar >= 0x20){
 			holdchar -=32;
-			tempweight += 0x0020;
+			tempweight += 0x20;
 		}
-		if(holdchar >= 16){
+		if(holdchar >= 0x10){
 			holdchar -= 16;
-			tempweight += 0x0010;
+			tempweight += 0x10;
 		}
-		if(holdchar >= 8){
+		if(holdchar >= 0x08){
 			holdchar -= 8;
-			tempweight += 0x0008;
+			tempweight += 0x08;
 		}
-		if(holdchar >= 4){
+		if(holdchar >= 0x04){
 			holdchar -= 4;
-			tempweight += 0x0004;
+			tempweight += 0x04;
 		}
-		if(holdchar >= 2){
+		if(holdchar >= 0x02){
 			holdchar -= 2;
-			tempweight += 0x0002;
+			tempweight += 0x02;
 		}
-		if(holdchar >= 1){
+		if(holdchar >= 0x01){
 			holdchar -= 1;
-			tempweight += 0x0001;
+			tempweight += 0x01;
 		}
 		if((PINA + PINB + PINC) > 140){
 			PORTD += 0x01;
@@ -71,7 +60,7 @@ int main(void) {
 		if(abs(PINA - PINC) > 80){
 			PORTD += 0x02;
 		}
-		PORTD += ((tempweight >> 4) & 0x00FC);
+		PORTD += (tempweight & 0xFC);
 	}
 	return 0;
 }
